a6c51b3ee52f8a8b5af0e16135e4d7f90d879bee
Niko Niyazi Türk <71526347+NikoNiyazi@users.noreply.github.com>
2022-04-10 08:02:02 +0300

    Custom Spammer (OPEN SOURCE)

--- spam.lua
+++ spam.lua
@@ -3,18 +3,19 @@ local curmsg = "[...] - Loading..."
 local mtype = "All"
 local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
 Event:FireServer(curmsg, mtype)
-wait(3)
+wait(1)
 curmsg = "[...] - Loaded!"
 Event:FireServer(curmsg, mtype)
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Boomhub | Custom Spammer", "Ocean")
+local Tab1 = Window:NewTab("Bypassed")
+local Section = Tab1:NewSection("Bypassed Spammer")
+local toggled = false
 -- LOADER --
 
 -- MAIN --
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Boomhub | Custom Spammer", "Ocean")
-local Tab1 = Window:NewTab("Spammer")
-local Section = Tab1:NewSection("Main")
-local toggled = false
-Section:NewTextBox("Enter Spam Text Here...", "spam the shit out of it :)))", function(txt)
+Section:NewTextBox("Enter Text Here...", "spam the shit out of it :)))", function(txt)
 	curmsg = txt
     print("[BOT] - Current Spam Text : "..curmsg)
 end)
@@ -36,3 +37,31 @@ Section:NewToggle("Spam", "", function(state)
     end
 end)
 -- MAIN --
+
+-- NORMAL --
+local Tab2 = Window:NewTab("Normal")
+local Section2 = Tab2:NewSection("Normal Spammer")
+local normsg = "Type Here"
+local nortoggled = false
+
+Section2:NewTextBox("Enter Text Here...", "spam the shit out of it :)))", function(txt)
+	normsg = txt
+    print("[BOT] - Current Spam Text : "..curmsg)
+end)
+
+Section2:NewToggle("Spam", "", function(state)
+    if state then
+        nortoggled = true
+        while true do
+            if nortoggled == true then
+                wait(1.1)
+                Event:FireServer(normsg, mtype)
+            else
+                break
+            end
+        end
+    else
+        nortoggled = false
+    end
+end)
+-- NORMAL --
