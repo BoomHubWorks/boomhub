3d0e93d7fc34581fccabbbd6e574cec2575233e7
Niko Niyazi Türk <71526347+NikoNiyazi@users.noreply.github.com>
2022-04-13 13:37:01 +0300

    Update tapping.lua

--- tapping.lua
+++ tapping.lua
@@ -3,15 +3,19 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
 local Window = Library.CreateLib("Boomhub | Tapping Simulator", "Ocean")
 local Tab1 = Window:NewTab("AutoFarm")
 local Section = Tab1:NewSection("Main")
+local Tab2 = Window:NewTab("Eggs")
+local Section2 = Tab2:NewSection("Main")
 --UI LOAD--
 
 --VARIABLES--
 local toggled = false
+local eggtog = false
+local egg = "Basic"
 
 
+--VARIABLES--
 
---VARIABLES
-
+--SCRIPT--
 Section:NewToggle("Autofarm", "", function(state)
     if state then
         toggled = true
@@ -27,3 +31,26 @@ Section:NewToggle("Autofarm", "", function(state)
         toggled = false
     end
 end)
+Section2:NewDropdown("Select Buy Egg", "", {"Basic", "Rare", "Secret"}, function(currentOption)
+    egg = currentOption
+end)
+Section2:NewToggle("Autobuy Eggs", "", function(state)
+    if state then
+        eggtog = true
+        while true do
+            if eggtog then
+                local args = {
+                    [1] = egg,
+                    [2] = "Single"
+                }
+                game:GetService("ReplicatedStorage").RemoteEvents.EggOpened:InvokeServer(unpack(args))
+                wait()
+            else
+                break
+            end
+        end
+    else
+        eggtog = false
+    end
+end)
+--SCRIPT--
